97a3a43939531e41b1e7309fc3a5290d7e73bc24
Nate Foss <natefoss@osiris.fossnet.net>
2015-03-06 21:44:57 -0800

    asdf

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
+/*
+ * term
+ * country
+ * media
+ * entity
+ * attribute
+ * callback
+ * limit
+ * lang
+ * version
+ * explicit
+ *
+ */
 $url = 'http://itunes.apple.com/search?';
 $search = 'term=Katy+Perry';
 $page = file_get_contents($url . $search);
-echo $page;
+$page = json_decode($page);
+
+$array = $page->results["0"];
+$version = $array->version;
+$artistname = $array->artistName;
+$artistid = $array->artistId;
+
+echo $version . "<br>";
+echo $artistname . "<br>";
+echo $artistid ."<br>";
 ?>
